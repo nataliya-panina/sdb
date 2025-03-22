b78cec051734424b7ad8a3c4ae73da85b5b93e5d
nataliya-panina <nataliya.sabathier@gmail.com>
2025-03-22 14:44:05 +0100

    Update consumer.py

--- 4/consumer.py
+++ 4/consumer.py
@@ -6,10 +6,8 @@ connection = pika.BlockingConnection(pika.ConnectionParameters('localhost'))
 channel = connection.channel()
 channel.queue_declare(queue='hello')
 
-
 def callback(ch, method, properties, body):
     print(" [x] Received %r" % body)
 
-
-channel.basic_consume(callback, queue='hello', no_ack=True)
+channel.basic_consume(on_message_callback=callback, queue='hello', auto_ack=True)
 channel.start_consuming()
