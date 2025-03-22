5884e96649da96b0967bbf3a4ab1b1b741362817
nataliya-panina <nataliya.sabathier@gmail.com>
2025-03-22 14:44:36 +0100

    Update producer.py

--- 4/producer.py
+++ 4/producer.py
@@ -3,6 +3,7 @@
 import pika
 
 connection = pika.BlockingConnection(pika.ConnectionParameters('localhost'))
+
 channel = connection.channel()
 channel.queue_declare(queue='hello')
 channel.basic_publish(exchange='', routing_key='hello', body='Hello Netology!')
